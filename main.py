f3583ea73f63b34aa7e37cda9e66b413e987afbf
Priyadarshi Roy <proy9714@gmail.com>
2023-12-06 13:40:31 +0530

    Including main.py in git push

---
+++ main.py
@@ -0,0 +1,11 @@
+from app import app, socketio
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    
+    # For local run
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, log_output=True)
+    
+    # For GCP
+    # socketio.run(app, debug=True, host='0.0.0.0', port=8080, log_output=True, allow_unsafe_werkzeug=True)
+    
