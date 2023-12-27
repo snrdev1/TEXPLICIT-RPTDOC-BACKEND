acbe81b212d6dc8c5e60821e7a4245fa59def1fa
Priyadarshi Roy <proy9714@gmail.com>
2023-12-27 13:15:01 +0530

    Reformatting

--- app/services/newsService.py
+++ app/services/newsService.py
@@ -19,6 +19,8 @@ from app.utils.llm.llm_summarizer import summarize_text
 from app.utils.production import Production
 from app.utils.web_scraper import WebScraper
 
+from ..utils.document import add_hyperlink
+
 
 class NewsService:
     @staticmethod
@@ -248,69 +250,7 @@ class NewsService:
         PRODUCTION_CHECK = Config.GCP_PROD_ENV
         if PRODUCTION_CHECK:
             bucket = Production.get_users_bucket()
-
-        def add_hyperlink(paragraph, text, url):
-            # This gets access to the document.xml.rels file and gets a new relation id value
-            part = paragraph.part
-            r_id = part.relate_to(
-                url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True
-            )
-
-            # Create the w:hyperlink tag and add needed values
-            hyperlink = docx.oxml.shared.OxmlElement("w:hyperlink")
-            hyperlink.set(
-                docx.oxml.shared.qn("r:id"),
-                r_id,
-            )
-
-            # Create a new run object (a wrapper over a 'w:r' element)
-            new_run = docx.text.run.Run(docx.oxml.shared.OxmlElement("w:r"), paragraph)
-            new_run.text = text
-
-            # Set the run's style to the builtin hyperlink style, defining it if necessary
-            new_run.style = get_or_create_hyperlink_style(part.document)
-            # Alternatively, set the run's formatting explicitly
-            # new_run.font.color.rgb = docx.shared.RGBColor(0, 0, 255)
-            # new_run.font.underline = True
-
-            # Join all the xml elements together
-            hyperlink.append(new_run._element)
-            paragraph._p.append(hyperlink)
-            return hyperlink
-
-        # This is only needed if you're using the builtin style above
-        def get_or_create_hyperlink_style(d):
-            """If this document had no hyperlinks so far, the builtin
-            Hyperlink style will likely be missing and we need to add it.
-            There's no predefined value, different Word versions
-            define it differently.
-            This version is how Word 2019 defines it in the
-            default theme, excluding a theme reference.
-            """
-            if "Hyperlink" not in d.styles:
-                if "Default Character Font" not in d.styles:
-                    ds = d.styles.add_style(
-                        "Default Character Font",
-                        docx.enum.style.WD_STYLE_TYPE.CHARACTER,
-                        True,
-                    )
-                    ds.element.set(docx.oxml.shared.qn("w:default"), "1")
-                    ds.priority = 1
-                    ds.hidden = True
-                    ds.unhide_when_used = True
-                    del ds
-                hs = d.styles.add_style(
-                    "Hyperlink", docx.enum.style.WD_STYLE_TYPE.CHARACTER, True
-                )
-                hs.base_style = d.styles["Default Character Font"]
-                hs.unhide_when_used = True
-                hs.font.color.rgb = docx.shared.RGBColor(0x05, 0x63, 0xC1)
-                hs.font.underline = True
-                del hs
-
-            return "Hyperlink"
-
-        # print("hello world")
+            
         try:
             title = news.get("title", "")
             summary = news.get("summary", "")
--- app/services/summaryService.py
+++ app/services/summaryService.py
@@ -28,6 +28,8 @@ from app.utils.common import Common
 from app.utils.llm.llm_highlights import generate_highlights
 from app.utils.llm.llm_summarizer import summarize_text
 
+from ..utils.document import add_hyperlink
+
 
 class SummaryService:
     # PUNKT_SENTENCE_TOKENIZER = nltk.data.load("app/models/english.pickle")
@@ -830,67 +832,6 @@ class SummaryService:
 
         # Add a paragraph with the document title
         document.add_paragraph("Itemized Summary", style="Title")
-
-        def add_hyperlink(paragraph, text, url):
-            # This gets access to the document.xml.rels file and gets a new relation id value
-            part = paragraph.part
-            r_id = part.relate_to(
-                url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True
-            )
-
-            # Create the w:hyperlink tag and add needed values
-            hyperlink = docx.oxml.shared.OxmlElement("w:hyperlink")
-            hyperlink.set(
-                docx.oxml.shared.qn("r:id"),
-                r_id,
-            )
-
-            # Create a new run object (a wrapper over a 'w:r' element)
-            new_run = docx.text.run.Run(docx.oxml.shared.OxmlElement("w:r"), paragraph)
-            new_run.text = text
-
-            # Set the run's style to the builtin hyperlink style, defining it if necessary
-            new_run.style = get_or_create_hyperlink_style(part.document)
-            # Alternatively, set the run's formatting explicitly
-            # new_run.font.color.rgb = docx.shared.RGBColor(0, 0, 255)
-            # new_run.font.underline = True
-
-            # Join all the xml elements together
-            hyperlink.append(new_run._element)
-            paragraph._p.append(hyperlink)
-            return hyperlink
-
-        # This is only needed if you're using the builtin style above
-        def get_or_create_hyperlink_style(d):
-            """If this document had no hyperlinks so far, the builtin
-            Hyperlink style will likely be missing and we need to add it.
-            There's no predefined value, different Word versions
-            define it differently.
-            This version is how Word 2019 defines it in the
-            default theme, excluding a theme reference.
-            """
-            if "Hyperlink" not in d.styles:
-                if "Default Character Font" not in d.styles:
-                    ds = d.styles.add_style(
-                        "Default Character Font",
-                        docx.enum.style.WD_STYLE_TYPE.CHARACTER,
-                        True,
-                    )
-                    ds.element.set(docx.oxml.shared.qn("w:default"), "1")
-                    ds.priority = 1
-                    ds.hidden = True
-                    ds.unhide_when_used = True
-                    del ds
-                hs = d.styles.add_style(
-                    "Hyperlink", docx.enum.style.WD_STYLE_TYPE.CHARACTER, True
-                )
-                hs.base_style = d.styles["Default Character Font"]
-                hs.unhide_when_used = True
-                hs.font.color.rgb = docx.shared.RGBColor(0x05, 0x63, 0xC1)
-                hs.font.underline = True
-                del hs
-
-            return "Hyperlink"
 
         def create_summary(title="", summary="", image_url=None, link=None):
             # Add a heading for the item
