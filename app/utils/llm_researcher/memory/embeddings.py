4d27d757210024f09cce0d1c7148951d19558d0d
Priyadarshi Roy <proy9714@gmail.com>
2024-02-06 12:09:47 +0530

    Error correction when using OpenAIEmbeddings

--- app/utils/llm_researcher/memory/embeddings.py
+++ app/utils/llm_researcher/memory/embeddings.py
@@ -4,7 +4,7 @@ from langchain.embeddings import OpenAIEmbeddings
 
 class Memory:
     def __init__(self, **kwargs):
-        self._embeddings = OpenAIEmbeddings()
+        self._embeddings = OpenAIEmbeddings(disallowed_special=())
 
     def get_embeddings(self):
         return self._embeddings
