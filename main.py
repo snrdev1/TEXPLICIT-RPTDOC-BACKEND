dfebd572cf70685a2262206e4b15abec91655ce3
snrdev1 <42143871+snrdev1@users.noreply.github.com>
2024-02-12 11:52:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     # app.run(debug=True)
     
     # For local run
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000, log_output=True)
+    # socketio.run(app, debug=True, host='0.0.0.0', port=5000, log_output=True)
     
     # For GCP
-    # socketio.run(app, debug=True, host='0.0.0.0', port=8080, log_output=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=8080, log_output=True, allow_unsafe_werkzeug=True)
     
