67cfb5bbe35599aae238b609a9ca0a0ee8d20fcf
Priyadarshi Roy <proy9714@gmail.com>
2024-03-21 16:37:20 +0530

    Defaulting to sequential subtopic report generation for passing previous headers

--- app/utils/llm_researcher/report_types/detailed_report/detailed_report.py
+++ app/utils/llm_researcher/report_types/detailed_report/detailed_report.py
@@ -25,7 +25,7 @@ class DetailedReport:
         subtopics: list,
         check_existing_report: bool,
         urls: List[str],
-        restrict_search: bool
+        restrict_search: bool,
     ):
         self.user_id = user_id
         self.task = task
@@ -40,9 +40,7 @@ class DetailedReport:
         self.restrict_search = restrict_search
         self.main_task_assistant = self._create_task_assistant()
         self.existing_headers = []
-        # This is a global variable to store the entire context accumulated at any point through searching and scraping
         self.global_context = []
-        # This is a global variable to store the entire url list accumulated at any point through searching and scraping
         self.global_urls = set(self.urls)
 
     async def generate_report(self) -> tuple:
@@ -54,11 +52,8 @@ class DetailedReport:
         # Conduct initial research using the main assistant
         await self._initial_research()
 
-        # Get list of all subtopics
-        subtopics = await self._get_all_subtopics()
-
-        # Generate the subtopic reports based on the subtopics gathered
-        _, subtopics_reports_body, tables = await self._generate_subtopic_reports(subtopics)
+        # Generate the subtopic reports sequentially
+        subtopics_reports_body, tables = await self._generate_subtopic_reports_async_sequential()
 
         # Construct the final list of unique tables
         self.main_task_assistant.tables_extractor.tables.extend(tables)
@@ -70,7 +65,7 @@ class DetailedReport:
         (
             detailed_report,
             detailed_report_path,
-            table_path
+            table_path,
         ) = await self._construct_detailed_report(subtopics_reports_body)
 
         return (
@@ -92,7 +87,7 @@ class DetailedReport:
             report_generation_id=self.report_generation_id,
             input_urls=self.urls,
             subtopics=self.subtopics,
-            restrict_search=self.restrict_search
+            restrict_search=self.restrict_search,
         )
 
     async def _check_existing_report(self) -> str:
@@ -121,102 +116,84 @@ class DetailedReport:
     async def _initial_research(self):
         # Conduct research using the main task assistant to gather content for generating subtopics
         await self.main_task_assistant.conduct_research(write_report=False)
-        # Update context of the global context variable
         self.global_context = self.main_task_assistant.context
-        # Update url list of the global list variable
         self.global_urls = self.main_task_assistant.visited_urls
 
-    async def _get_all_subtopics(self) -> list:
-        subtopics = await self.main_task_assistant.get_subtopics()
-        return subtopics.dict()["subtopics"]
-
-    async def _generate_subtopic_reports(self, subtopics: list) -> tuple:
-        reports = []
-        report_body = ""
+    async def _generate_subtopic_reports_async_sequential(self) -> tuple:
+        reports_body = ""
         tables = []
+        existing_headers = []
 
-        async def fetch_report(subtopic):
+        async def generate_subtopic_report(subtopic):
+            nonlocal existing_headers  # Use the outer existing_headers list
+
+            current_subtopic_task = subtopic.get("task")
+            subtopic_source = subtopic.get("source")
+            subtopic_assistant = ResearchAgent(
+                user_id=self.user_id,
+                query=current_subtopic_task,
+                source=subtopic_source,
+                format=self.format,
+                report_type=Enumerator.ReportType.SubtopicReport.value,
+                websocket=self.websocket,
+                parent_query=self.task,
+                subtopics=self.subtopics,
+                report_generation_id=self.report_generation_id,
+                restrict_search=self.restrict_search,
+                visited_urls=self.global_urls,
+                agent=self.main_task_assistant.agent,
+                role=self.main_task_assistant.role,
+            )
+
+            # The subtopics should start research from the context gathered till now
+            subtopic_assistant.context = list(set(self.global_context))
+
+            if self.restrict_search:
+                # If search type is restricted, then the existing context should be used for writing the report
+                report_markdown = await subtopic_assistant.write_report(
+                    existing_headers=existing_headers
+                )
+            else:
+                # If search type is mixed, then further research needs to be conducted
+                report_markdown = await subtopic_assistant.conduct_research(
+                    max_docs=10, score_threshold=1, existing_headers=existing_headers
+                )
+
+            # Update context of the global context variable
+            self.global_context = list(set(subtopic_assistant.context))
+            # Update url list of the global list variable
+            self.global_urls.update(subtopic_assistant.visited_urls)
+
+            # Append the headers to the existing_headers list
+            existing_headers.append(
+                {
+                    "subtopic task": current_subtopic_task,
+                    "headers": extract_headers(report_markdown),
+                }
+            )
+
+            if not report_markdown.strip():
+                print(
+                    f"⚠️ Failed to gather data from research on subtopic : {self.task}"
+                )
+                return "", []
+
+            return report_markdown, subtopic_assistant.tables_extractor.tables
+
+        # Generate the subtopic reports asynchronously in a sequential manner
+        for subtopic in self.subtopics:
             emit_report_status(
                 self.user_id,
                 self.report_generation_id,
-                f"🚦 Conducting research on subtopic : {subtopic}...",
+                f"🚦 Conducting research on subtopic : {subtopic['task']}...",
             )
-            (
-                subtopic_report_markdown,
-                subtopic_path,
-                subtopic_tables
-            ) = await self._get_subtopic_report(
+            subtopic_report_markdown, subtopic_tables = await generate_subtopic_report(
                 subtopic
             )
-            return {
-                "topic": subtopic,
-                "markdown_report": subtopic_report_markdown,
-                "path": subtopic_path,
-                "tables": subtopic_tables
-            }
+            reports_body += "\n\n\n" + subtopic_report_markdown
+            tables.extend(subtopic_tables)
 
-        tasks = [fetch_report(subtopic) for subtopic in subtopics]
-        results = await asyncio.gather(*tasks)
-
-        for result in filter(lambda r: r["markdown_report"], results):
-            reports.append(result)
-            report_body += "\n\n\n" + result["markdown_report"]
-            tables.extend(result["tables"])
-
-        return reports, report_body, tables
-
-    async def _get_subtopic_report(self, subtopic: dict) -> tuple:
-        current_subtopic_task = subtopic.get("task")
-        subtopic_source = subtopic.get("source")
-        subtopic_assistant = ResearchAgent(
-            user_id=self.user_id,
-            query=current_subtopic_task,
-            source=subtopic_source,
-            format=self.format,
-            report_type=Enumerator.ReportType.SubtopicReport.value,
-            websocket=self.websocket,
-            parent_query=self.task,
-            subtopics=self.subtopics,
-            report_generation_id=self.report_generation_id,
-            restrict_search=self.restrict_search,
-            visited_urls=self.global_urls,
-            agent=self.main_task_assistant.agent,
-            role=self.main_task_assistant.role
-        )
-
-        # The subtopics should start research from the context gathered till now
-        subtopic_assistant.context = list(set(self.global_context))
-
-        if self.restrict_search:
-            # If search type is restricted then the existing context should be used for writing report
-            report_markdown = await subtopic_assistant.write_report(
-                existing_headers=self.existing_headers
-            )
-        else:
-            # If search type is mixed then further research needs to be conducted
-            report_markdown = await subtopic_assistant.conduct_research(
-                max_docs=10, score_threshold=1, existing_headers=self.existing_headers
-            )
-
-        # Update context of the global context variable
-        self.global_context = list(set(subtopic_assistant.context))
-        # Update url list of the global list variable
-        self.global_urls.update(subtopic_assistant.visited_urls)
-
-        # After a subtopic report has been generated then append the headers of the report to existing headers
-        self.existing_headers.append(
-            {
-                "subtopic task": current_subtopic_task,
-                "headers": extract_headers(report_markdown),
-            }
-        )
-
-        if not report_markdown.strip():
-            print(
-                f"⚠️ Failed to gather data from research on subtopic : {self.task}")
-            return "", "", []
-
-        return report_markdown, "", subtopic_assistant.tables_extractor.tables
+        return reports_body, tables
 
     async def _construct_detailed_report(self, report_body: str) -> tuple:
         # Get the introduction and the conclusion
@@ -225,12 +202,14 @@ class DetailedReport:
         # Construct the final detailed report
         detailed_report = (
             introduction + "\n\n"
-            + table_of_contents(report_body + '\n\n' + conclusion) + "\n\n"
+            + table_of_contents(report_body + "\n\n" + conclusion) + "\n\n"
             + report_body + "\n\n"
             + conclusion
         )
 
         # Get the detailed report path and the table path
-        detailed_report_path, table_path = await self.main_task_assistant.save_report(detailed_report)
+        detailed_report_path, table_path = await self.main_task_assistant.save_report(
+            detailed_report
+        )
 
         return detailed_report, detailed_report_path, table_path
